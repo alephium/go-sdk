91be3edb8c3271bed91a84636b42020a239fa929
lbqds <lbqds@outlook.com>
2022-06-20 11:37:00 +0800

    Add update script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION=v1.4.1
+curl https://raw.githubusercontent.com/alephium/alephium/${VERSION}/api/src/main/resources/openapi.json -o openapi.json
+openapi-generator generate -i ./openapi.json -g go -o ./ --skip-validate-spec --package-name alephium --git-repo-id go-sdk --git-user-id alephium
+rm openapi.json
+rm git_push.sh
+
